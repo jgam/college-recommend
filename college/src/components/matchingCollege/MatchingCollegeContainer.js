ff2965d8d68bd0003e09f49346eefdf07ea937b0
jgam <jgam@alumni.nd.edu>
2020-05-05 01:17:30 +0900

    college component without API done

--- college/src/components/matchingCollege/MatchingCollegeContainer.js
+++ college/src/components/matchingCollege/MatchingCollegeContainer.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MatchingCollegePresenter from './MatchingCollegePresenter';
+
+const initialFormData = {
+  act: 0,
+  sat: 0,
+  gpa: 0,
+  colleges: [],
+};
 
 function MatchingCollegeContainer() {
   console.log('currently in container');
+  const [formData, setFormData] = useState(initialFormData);
+  //use api
+
+  function getAPI() {
+    return 'done!';
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log('im in handlesubmit');
+    const data = await getAPI();
+    console.log(data);
+    //update colleges with setFormdata
+    //reset the buttons
+    setFormData(initialFormData);
+  };
+
+  const handleChange = (e) => {
+    console.log(e.target);
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  console.log(formData);
+
   return (
     <div>
-      <MatchingCollegePresenter />
+      <MatchingCollegePresenter
+        formData={formData}
+        handleChange={handleChange}
+        handleSubmit={handleSubmit}
+      />
     </div>
   );
 }
--- college/src/components/matchingCollege/MatchingCollegePresenter.js
+++ college/src/components/matchingCollege/MatchingCollegePresenter.js
@@ -3,8 +3,38 @@ import React from 'react';
 //input field for sat scores or act scores
 //input field for gpa
 
-function MatchingCollegePresenter() {
-  return <div>currently in matchingCollegePresenter</div>;
+function MatchingCollegePresenter({ formData, handleChange, handleSubmit }) {
+  //formData.colleges = list of colleges and we should show them
+  return (
+    <div>
+      currently in matchingCollegePresenter
+      <h1>tell me ur stats!</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type={'text'}
+          placeholder={'type in act score'}
+          value={formData.act}
+          name={'act'}
+          onChange={handleChange}
+        />
+        <input
+          type={'text'}
+          placeholder={'type in sat score'}
+          value={formData.sat}
+          name={'sat'}
+          onChange={handleChange}
+        />
+        <input
+          type={'text'}
+          placeholder={'type in GPA'}
+          value={formData.gpa}
+          name={'gpa'}
+          onChange={handleChange}
+        />
+        <button type={'submit'}> Submit </button>
+      </form>
+    </div>
+  );
 }
 
 export default MatchingCollegePresenter;
