c8df27aa946e0dcd1b2292fe6d51336435bbf344
jgam <jgam@alumni.nd.edu>
2020-05-22 18:12:11 +0900

    init

--- college/src/components/matchingCollege/MatchingCollegeContainer.js
+++ college/src/components/matchingCollege/MatchingCollegeContainer.js
@@ -6,7 +6,8 @@ const initialFormData = {
   act: '',
   sat: '',
   gpa: '',
-  colleges: [],
+  colleges: {},
+  loading: true,
 };
 
 function MatchingCollegeContainer() {
@@ -16,6 +17,7 @@ function MatchingCollegeContainer() {
   //use api
   async function getAPI(formData) {
     const params = {
+      //two things: info_ids & filters
       filters: {
         schoolSize: ['large'],
         zipCode: '02111',
@@ -28,21 +30,30 @@ function MatchingCollegeContainer() {
       'https://api.collegeai.com/v1/api/college-list?api_key=free_c2f12782a8449751c2c15f5891',
       { params }
     );
-    return colleges;
+    return colleges.data.colleges;
   }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
     console.log('im in handlesubmit');
-    const data = await getAPI(formData);
-    console.log(data);
-    //update colleges with setFormdata
-    //reset the buttons
-    setFormData(initialFormData);
+
+    try {
+      const data = await getAPI(formData);
+      //update colleges with setFormdata
+      //reset the buttons
+      setFormData({ ...formData, colleges: data });
+      console.log('this should be after college data');
+    } catch (error) {
+      console.log(error);
+    } finally {
+      console.log('in finally');
+      console.log(formData);
+      setFormData({ ...formData, loading: false });
+    }
   };
 
   const handleChange = (e) => {
-    e.preventDefault();
     console.log(e.target);
     setFormData({
       ...formData,
