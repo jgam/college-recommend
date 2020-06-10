5e78ec6b71402497a4c296b3849abffe380f2f2b
jgam <jgam@alumni.nd.edu>
2020-06-10 14:45:10 +0900

    [styling] homepage styling. hover needs to be enhacned

--- college/src/components/Home/HomePresenter.js
+++ college/src/components/Home/HomePresenter.js
@@ -10,9 +10,9 @@ import Slider4 from '../../img/slider4.jpg';
 function HomePresenter() {
   //check webtoken to say hello or login button
   const Wrapper = styled.div`
-    margin-top: 30px;
+    margin-top: 50px;
     display: flex;
-    justify-content: center;
+    justify-content: space-around;
     width: 80%;
     margin-right: auto;
     margin-left: auto;
@@ -28,6 +28,26 @@ function HomePresenter() {
   const Img = styled.img`
     max-height: 100%;
     max-width: 100%;
+  `;
+
+  const CriteriaBox = styled.div`
+    width: 300px;
+    height: 400px;
+    background-color: rgba(103, 58, 183, 0.5);
+    border-radius: 30px;
+    text-align: center;
+    font-size: 30px;
+    color: white;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    &:hover {
+      background-color: rgba(103, 58, 183, 1);
+    }
+  `;
+
+  const ContentsBox = styled.div`
+    font-size: 20px;
   `;
 
   const LeftTitle = styled.div``;
@@ -53,9 +73,27 @@ function HomePresenter() {
           <Item>My College?</Item>
         </Carousel>
       </Wrapper>
-      <Wrapper>Academics?</Wrapper>
-      <Wrapper>Sports?</Wrapper>
-      <Wrapper>City?</Wrapper>
+      <Wrapper>
+        <CriteriaBox>
+          Academics?
+          <ContentsBox>Curriculum</ContentsBox>
+          <ContentsBox>Career path</ContentsBox>
+          <ContentsBox>Number of majors</ContentsBox>
+        </CriteriaBox>
+        <CriteriaBox>
+          Sports?
+          <ContentsBox>Sports opportunities</ContentsBox>
+          <ContentsBox>Sports facilities</ContentsBox>
+          <ContentsBox>intermullar activities</ContentsBox>
+        </CriteriaBox>
+        <CriteriaBox>
+          City?
+          <ContentsBox>City vibes</ContentsBox>
+          <ContentsBox>Weather</ContentsBox>
+          <ContentsBox>What is there?</ContentsBox>
+        </CriteriaBox>
+      </Wrapper>
+      <Wrapper>even more to it!</Wrapper>
     </>
   );
 }
