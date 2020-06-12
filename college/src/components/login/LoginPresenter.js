6aef38bca9b09b4391182867d155155a9016aa33
jgam <jgam@alumni.nd.edu>
2020-06-12 22:34:57 +0900

    [colleges] styling done

--- college/src/components/login/LoginPresenter.js
+++ college/src/components/login/LoginPresenter.js
@@ -1,31 +1,90 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  height: 400px;
+  width: 300px;
+  box-sizing: border-box;
+  color: white;
+  background-color: black;
+  border-radius: 20px;
+  padding: 20px;
+  margin: 30px auto;
+`;
+
+const Title = styled.div`
+  font-size: 30px;
+  font-weight: 400;
+  margin-left: 23px;
+`;
+
+const Button = styled.button`
+  border-radius: 30px;
+  background-color: #57b846;
+  color: white;
+  display: block;
+  font-size: 20px;
+  width: 110px;
+  height: 35px;
+  border: none;
+  cursor: pointer;
+  &:hover {
+    opacity: 0.7;
+  }
+`;
+
+const Input = styled.input`
+  display: block;
+  height: 20px;
+  background-color: black;
+  border: none;
+  font-size: 20px;
+  border-bottom: solid grey 1px;
+  color: white;
+  &:focus {
+    outline: none;
+  }
+`;
+
+const Form = styled.form`
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+`;
 
 function LoginPresenter({ loginData, handleChange, handleSubmit }) {
   return (
-    <div>
-      Hello, this is login
-      <form onSubmit={handleSubmit}>
-        <input
+    <Wrapper>
+      <Title>Login Info</Title>
+      <Form onSubmit={handleSubmit}>
+        <Input
           type={'text'}
           placeholder={'type in id'}
           value={loginData.id}
           name={'username'}
           onChange={handleChange}
         />
-        <input
+        <Input
           type={'password'}
           placeholder={'type in pw'}
           value={loginData.pw}
           name={'password'}
           onChange={handleChange}
         />
-        <button>login</button>
-        <button>
-          <Link to={'/signup'}>Sign up</Link>
-        </button>
-      </form>
-    </div>
+        <Button typre={'submit'}>login</Button>
+        <Button>
+          <Link
+            to={'/signup'}
+            style={{ textDecoration: 'inherit', color: 'inherit' }}
+          >
+            Sign up
+          </Link>
+        </Button>
+      </Form>
+    </Wrapper>
   );
 }
 
--- college/src/components/signup/SignupPresenter.js
+++ college/src/components/signup/SignupPresenter.js
@@ -1,34 +1,88 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const Wrapper = styled.div`
+  height: 400px;
+  width: 300px;
+  box-sizing: border-box;
+  color: white;
+  background-color: black;
+  border-radius: 20px;
+  padding: 20px;
+  margin: 30px auto;
+  z-index: 1;
+`;
+
+const Title = styled.div`
+  font-size: 30px;
+  font-weight: 400;
+  margin-left: 23px;
+`;
+
+const Button = styled.button`
+  border-radius: 30px;
+  background-color: #57b846;
+  color: white;
+  display: block;
+  font-size: 20px;
+  width: 110px;
+  height: 35px;
+  border: none;
+  cursor: pointer;
+  &:hover {
+    opacity: 0.7;
+  }
+`;
+
+const Input = styled.input`
+  display: block;
+  height: 20px;
+  background-color: black;
+  border: none;
+  font-size: 20px;
+  border-bottom: solid grey 1px;
+  color: white;
+  &:focus {
+    outline: none;
+  }
+`;
+
+const Form = styled.form`
+  height: 300px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+`;
 function SignupPresenter({ SignupData, handleChange, handleSubmit }) {
   return (
-    <div>
-      Hello, im signup presenter
-      <form onSubmit={handleSubmit}>
-        <input
+    <Wrapper>
+      <Title>Signup Info</Title>
+      <Form onSubmit={handleSubmit}>
+        <Input
           type={'text'}
           placeholder={'type in username'}
           value={SignupData.useranme}
           name={'username'}
           onChange={handleChange}
         />
-        <input
+        <Input
           type={'password'}
           placeholder={'type in pw'}
           value={SignupData.password}
           name={'password'}
           onChange={handleChange}
         />
-        <input
+        <Input
           type={'text'}
           placeholder={'type in name'}
           value={SignupData.name}
           name={'name'}
           onChange={handleChange}
         />
-        <button onSubmit={handleSubmit}>Signup</button>
-      </form>
-    </div>
+        <Button onSubmit={handleSubmit}>Register</Button>
+      </Form>
+    </Wrapper>
   );
 }
 
