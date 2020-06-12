838f21a0533adc7bd22fd49fae02ab98ff78e895
jonsoku2 <whdtjr2792@gmail.com>
2020-06-12 23:18:11 +0900

    1차

---
+++ college/server/server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const config = require("./config");
+const morgan = require("morgan");
+const cors = require("cors");
+
+//express
+const app = express();
+const port = process.env.PORT || 5000;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(morgan("dev"));
+app.set("jwt-secret", config.secret);
+//mongoose connection
+var mongoose = require("mongoose");
+
+//default mongoose connection
+//mongoDB = college is the database name, and under their, we have collections
+var mongoDB =
+  "mongodb+srv://jgam:19921019@cluster0-ewm3u.mongodb.net/college?retryWrites=true&w=majority";
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", function () {
+  console.log("MongoDB database connection established successfully");
+});
+
+//mongoose schema
+var user = require("./models/userModel");
+console.log("userModel");
+console.log(user);
+
+//send schema to router
+var router = require("./routes")(app, user);
+//for api
+app.use("/api", require("./routes/api"));
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
--- college/src/components/Home/HomeContainer.js
+++ college/src/components/Home/HomeContainer.js
@@ -1,27 +1,31 @@
-import React, { useContext, useEffect } from 'react';
-import HomePresenter from './HomePresenter';
+import React, { useContext, useEffect, useCallback } from "react";
+import HomePresenter from "./HomePresenter";
 
-import AuthContext from '../../contexts/AuthContext';
-import { saveToken } from '../../auth/tokens';
-import { getLoginInfo } from '../../components/api/backendAPI';
+import AuthContext from "../../contexts/AuthContext";
+import { saveToken } from "../../auth/tokens";
+import { getLoginInfo } from "../../components/api/backendAPI";
 
 function HomeContainer() {
   const { setAuth, setColleges } = useContext(AuthContext);
 
-  async function getLogin(token) {
-    saveToken(token);
-    const value = await getLoginInfo({ token });
-    setAuth(true, value.data.info._id);
-    setColleges(value.data.info.colleges);
-  }
+  const getLogin = useCallback(
+    async (token) => {
+      saveToken(token);
+      const value = await getLoginInfo({ token });
+      setAuth(true, value.data.info._id);
+      setColleges(value.data.info.colleges);
+    },
+    [setAuth, setColleges]
+  );
+
   useEffect(() => {
-    if (window.document.cookie.startsWith('token=')) {
+    if (window.document.cookie.startsWith("token=")) {
       getLogin(window.document.cookie.slice(6));
       //here check if statement with token
     } else {
       setAuth(false);
     }
-  }, []);
+  }, [getLogin, setAuth]);
 
   //here checking the token and set state to logged in!
 
